7764a89454c31fd92c0d84a98869f33380094dca
ataysopu <sopuevatay@gmail.com>
2021-01-06 14:21:04 +0300

    created repository

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import axios from "axios";
+
+export const App = () => {
+    let [products, setProducts] = React.useState([])
+    React.useEffect(() => {
+        let fetchData = async () => {
+            let result = await axios.get(
+                'api/todos',
+            );
+            setProducts(result.data)
+        };
+        fetchData();
+    }, [])
+
+    let renderProds = products?.map(item => <div>{item.name}</div>)
+
+    return (
+        <div>
+            {renderProds}
+        </div>
+    );
+};
